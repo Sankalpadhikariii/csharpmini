e73054b7b83309667fe6a73d65000d846c104c55
Sankalpadhikariii <115810533+Sankalpadhikariii@users.noreply.github.com>
2022-12-13 21:37:29 +0545

    Add files via upload

---
+++ dd.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_taking_App
+{
+    static class Program
+    {
+        
+        
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
